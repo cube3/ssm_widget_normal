738884bb034809093e7dd862b504ed79ed8f3c4e
Igor <cube3workshop@gmail.com>
2015-07-14 23:43:09 +0300

    Working carousel widget

--- js/carousel.setup.js
+++ js/carousel.setup.js
@@ -1,21 +1,20 @@
 $(document).ready(function(){
 
-$('.products-holder').slick({
-  infinite: false,
-  draggable: false,
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  dots: true,
-  appendDots: $(".section-header"),
-  dotsClass: "nav-bullet",
-  customPaging: function(slider, i) {
-        return "";
-    },
-  prevArrow: $(".prevArrow"),
-  nextArrow: $(".nextArrow")
+  $('.products-holder').slick({
+    
+    infinite: false,
+    draggable: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: true,
+    appendDots: $(".section-header"),
+    dotsClass: "nav-bullet",
+    customPaging: function(slider, i) {
+          return "";
+      },
+    prevArrow: $(".prevArrow"),
+    nextArrow: $(".nextArrow")
 
-
-
-});
+  });
 
 });
